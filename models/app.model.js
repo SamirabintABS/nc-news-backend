b4f76b1882ae58cd8ade087a6bcf902d4a840892
SamirabintABS <68996388+SamirabintABS@users.noreply.github.com>
2023-03-29 11:55:28 +0100

    changed comment count to be a number instead of string

--- models/app.model.js
+++ models/app.model.js
@@ -27,7 +27,7 @@ exports.fetchAllArticles = () => {
         articles.created_at,
         articles.votes,
         articles.article_img_url, 
-        COUNT(comments.comment_id) AS comment_count
+        COUNT(comments.comment_id)::INT AS comment_count
         FROM articles
         LEFT JOIN comments
         ON articles.article_id = comments.article_id 
