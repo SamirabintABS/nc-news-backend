100c585932c5b6e6f4153a3ce25e899f548effef
SamirabintABS <68996388+SamirabintABS@users.noreply.github.com>
2023-05-18 20:35:25 +0100

    generate endpoints.json

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { getTopics, getArticlesById, getArticles, getCommentsById, addComments, updateVotes, deleteComments, getAllUsers } = require('./controllers/app.controller');
 const { handlePsqlErrors, handleServerErrors, handleCustomErrors } = require('./errors/index.js')
+const fs = require('fs');
 
 const app = express();
 
@@ -30,6 +31,29 @@ app.get("/api/users", getAllUsers)
 app.all("*", (req, res, next) => {
     res.status(404).send({ msg: 'Path not found' })
 })
+
+// Define a route for generating endpoints.json
+app.get('/generate-endpoints', (req, res) => {
+  const routes = app._router.stack // Get all registered routes
+    .filter((r) => r.route) // Filter out middleware
+    .map((r) => {
+      return {
+        path: r.route.path,
+        methods: Object.keys(r.route.methods),
+      };
+    });
+
+  const endpoints = { endpoints: routes };
+
+  fs.writeFile('endpoints.json', JSON.stringify(endpoints, null, 2), (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error generating endpoints.json');
+    } else {
+      res.send('endpoints.json generated successfully!');
+    }
+  });
+});
 
 app.use(handlePsqlErrors);
 app.use(handleCustomErrors);
